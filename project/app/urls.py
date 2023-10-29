b7ebcbdf63546d922f2f6b83999e36e9af4a0db9
Samruddhi Kashmire <104366974+samruddhikashmire@users.noreply.github.com>
2023-10-29 22:49:28 +0530

    Add files via upload

---
+++ project/app/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path,include
+from . import views
+urlpatterns = [
+    path("home/" , views.HomePage,name="home"),
+    path("" , views.SignupPage,name="Signup"),
+    path("register/" , views.RegisterUser,name='register'),
+    path("otppage/" , views.OtpPage,name='otppage'),
+    path("otp/" , views.OtpVerify , name = 'otp'),
+    path("loginpage/" , views.LoginPage , name = 'loginpage'),
+    path("loginuser/" , views.LoginUser, name = 'login'),
+    path("admission/" , views.ProfilePage, name = 'admission'),
+    path("updateprofile/" , views.UpdateProfile, name = 'updateprofile'),
+    path("aboutus/" , views.AboutusPage, name= 'aboutus'),
+
+
+    
+
+
+
+]
